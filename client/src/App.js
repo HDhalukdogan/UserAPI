fc4f3d071951e6848d8e97ba0ff6e798db169630
HDhalukdogan <hd.halukdogan@hotmail.com>
2023-04-09 09:33:30 +0300

    react client

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import { useSelector } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from 'routes';
+
+// defaultTheme
+import themes from 'themes';
+
+// project imports
+import NavigationScroll from 'layout/NavigationScroll';
+import { useEffect } from 'react';
+import agent from 'api/agent';
+
+// ==============================|| APP ||============================== //
+
+const App = () => {
+    const customization = useSelector((state) => state.customization);
+    const getUsers = async () => {
+        const users = await agent.Account.getAllUser().then(res=>res);
+        console.log('users', users)
+    }
+    useEffect(() => {
+       getUsers()
+    }, [])
+    
+
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes(customization)}>
+                <CssBaseline />
+                <NavigationScroll>
+                    <Routes />
+                </NavigationScroll>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+};
+
+export default App;
---
+++ client/src/api/agent.js
@@ -0,0 +1,32 @@
+import axios  from "axios";
+
+axios.defaults.baseURL = 'http://localhost:5000/api/';
+
+
+const responseBody = (response) => response.data;
+
+const request = {
+    get: (url, params) => axios.get(url, {params}).then(responseBody),
+    post: (url, body) => axios.post(url, body).then(responseBody),
+    put: (url, body) => axios.put(url, body).then(responseBody),
+    delete: (url) => axios.delete(url).then(responseBody),
+    postForm: (url, data) => axios.post(url, data, {
+        headers: {'Content-type': 'multipart/form-data'}
+    }).then(responseBody),
+    putForm: (url, data) => axios.put(url, data, {
+        headers: {'Content-type': 'multipart/form-data'}
+    }).then(responseBody)
+}
+
+const Account = {
+    login: (values) => request.post('account/login', values),
+    register: (values) => request.post('account/register', values),
+    currentUser: () => request.get('account/currentUser'),
+    getAllUser: ()=>request.get("account/getAllUser")
+}
+
+const agent = {
+    Account
+}
+
+export default agent;
